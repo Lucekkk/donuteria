bdd1d42d370ed460f9c35da13b6caa2c54dd634c
Raven <nowakowski920@gmail.com>
2026-01-11 19:19:56 +0100

    addingUseStateToThreeProdsComponent

--- components/products-home/three-products.js
+++ components/products-home/three-products.js
@@ -1,23 +1,80 @@
+'use client';
 import Image from "next/image";
 import classes from "./three-products.module.css";
+import { useState } from "react";
+import Link from "next/link";
+ 
 
 export default function ThreeProducts({products}){
+     const [active, setActive] = useState(0);
+     const [description, setDescription] = useState(
+    products.map((product, index) => (
+      <p
+        key={product.id}
+        className={
+          index === 0
+            ? `${classes.description} ${classes.active}`
+            : classes.description
+        }
+      >
+        {product.opis}
+      </p>
+    ))
+  );
+
+  const donutHandler = (newIndex) => {
+    setActive(newIndex);
+
+    setDescription(
+      products.map((product, index) => (
+        <p
+          key={product.id}
+          className={
+            index === newIndex
+              ? `${classes.description} ${classes.active}`
+              : classes.description
+          }
+        >
+          {product.opis}
+        </p>
+      ))
+    );
+  };
     return(
          
-      
-    //   <div>
-    //       {products.map(product => (
-    //         <div key={product.id}>
-    //             <p>{product.nazwa}</p>
-    //             <p>{product.opis}</p>
-    //             <div className={classes['div-image']}>
-    //               <Image src={product.obrazek} alt={product.opis_obrazka} sizes="(max-width: 768px) 100vw, 33vw" fill priority/>
-    //             </div>
-    //         </div>
-    //       ))}
-    //   </div>
 
-      <main>
+      <main className={classes.main}>
+        <div className={classes.leftPanel}>
+            <div className={classes.viewport}>
+                <div
+                className={classes.slider}
+                style={{ transform: `translateY(-${active * 150}px)` }}
+                >
+                    {products.map(product => (
+                        <h1 key={product.id} className={`${classes.slide} ${'balooFont'}`}>{product.nazwa}</h1>
+                    ))}
+                </div>
+            </div>
+
+             <div className={classes.descriptionBox}>
+                {description}
+             </div>
+
+             <div className={classes.links}>
+                <Link href='/'>Zamów teraz</Link>
+                <Link href='/'>Więcej</Link>
+             </div>
+
+            <div className={classes.donutsBtnsContainer}>
+                    {products.map((prod, index) => (
+                        <button key={prod.id} onClick={() => donutHandler(index)}>
+                            <Image src={prod.obrazek} alt={prod.opis_obrazka} priority fill/>
+                        </button>
+                    ))}
+            </div>
+            
+        </div>
+        <div className={classes.rightPanel}></div>
 
       </main>
    
