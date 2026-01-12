05f30e15c591c379ee52334f539512b9c6867e29
Raven <nowakowski920@gmail.com>
2026-01-12 17:22:05 +0100

    Improving donut animation

--- components/products-home/three-products.js
+++ components/products-home/three-products.js
@@ -1,14 +1,13 @@
-'use client';
+"use client";
 import Image from "next/image";
 import classes from "./three-products.module.css";
 import { useState } from "react";
 import Link from "next/link";
- 
 
-export default function ThreeProducts({products}){
-     const [active, setActive] = useState(0);
-     const [backgroundColor, setBackgroundColor] = useState('#EA7BBE')
-     const [description, setDescription] = useState(
+export default function ThreeProducts({ products }) {
+  const [active, setActive] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState("#EA7BBE");
+  const [description, setDescription] = useState(
     products.map((product, index) => (
       <p
         key={product.id}
@@ -24,19 +23,19 @@ export default function ThreeProducts({products}){
   );
   const [donutBtn, setDonutBtn] = useState(
     products.map((prod, index) => (
-      <button 
-        key={prod.id} 
+      <button
+        key={prod.id}
         onClick={() => donutHandler(index)}
-        className={ index === 0
-          ? `${classes.donutsBtn} ${classes.active}`
+        className={
+          index === 0
+            ? `${classes.donutsBtn} ${classes.active}`
             : classes.donutsBtn
         }
-        >
-          <Image src={prod.obrazek} alt={prod.opis_obrazka} priority fill/>
+      >
+        <Image src={prod.obrazek} alt={prod.opis_obrazka} priority fill />
       </button>
-          )))
-
-
+    ))
+  );
 
   const donutHandler = (newIndex) => {
     setActive(newIndex);
@@ -55,61 +54,72 @@ export default function ThreeProducts({products}){
         </p>
       ))
     );
-    if(newIndex === 0){
-      setBackgroundColor('#EA7BBE');
-    }else if(newIndex === 1){
-      setBackgroundColor('#4ABAF0');
-    }else{
-      setBackgroundColor('#974F2D')
+    if (newIndex === 0) {
+      setBackgroundColor("#EA7BBE");
+    } else if (newIndex === 1) {
+      setBackgroundColor("#4ABAF0");
+    } else {
+      setBackgroundColor("#974F2D");
     }
 
     setDonutBtn(
       products.map((prod, index) => (
-        <button 
-          key={prod.id} 
+        <button
+          key={prod.id}
           onClick={() => donutHandler(index)}
-          className={ index === newIndex
-            ? `${classes.donutsBtn} ${classes.active}`
+          className={
+            index === newIndex
+              ? `${classes.donutsBtn} ${classes.active}`
               : classes.donutsBtn
           }
-          >
-            <Image src={prod.obrazek} alt={prod.opis_obrazka} priority fill/>
+        >
+          <Image src={prod.obrazek} alt={prod.opis_obrazka} priority fill />
         </button>
-          )))
+      ))
+    );
   };
-    return(
-         
-
-      <main className={classes.main}>
-        <div className={classes.leftPanel}>
-            <div className={classes.viewport}>
-                <div
-                className={classes.slider}
-                style={{ transform: `translateY(-${active * 150}px)` }}
-                >
-                    {products.map(product => (
-                        <h1 key={product.id} className={`${classes.slide} ${'balooFont'}`}>{product.nazwa}</h1>
-                    ))}
-                </div>
-            </div>
-
-             <div className={classes.descriptionBox}>
-                {description}
-             </div>
-
-             <div className={classes.links}>
-                <Link href='/' style={{backgroundColor: `${backgroundColor}`}}>Zamów teraz</Link>
-                <Link href='/' style={{backgroundColor: `${backgroundColor}`}}>Więcej</Link>
-             </div>
-
-            <div className={classes.donutsBtnsContainer}>
-                    {donutBtn}
-            </div>
-            
+  return (
+    <main className={classes.main}>
+      <div className={classes.leftPanel}>
+        <div className={classes.viewport}>
+          <div
+            className={classes.slider}
+            style={{ transform: `translateY(-${active * 150}px)` }}
+          >
+            {products.map((product) => (
+              <h1
+                key={product.id}
+                className={`${classes.slide} ${"balooFont"}`}
+              >
+                {product.nazwa}
+              </h1>
+            ))}
+          </div>
         </div>
-        <div className={classes.rightPanel}></div>
 
-      </main>
-   
-    )
+        <div className={classes.descriptionBox}>{description}</div>
+
+        <div className={classes.links}>
+          <Link href="/" style={{ backgroundColor: `${backgroundColor}` }}>
+            Zamów teraz
+          </Link>
+          <Link href="/" style={{ backgroundColor: `${backgroundColor}` }}>
+            Więcej
+          </Link>
+        </div>
+
+        <div className={classes.donutsBtnsContainer}>{donutBtn}</div>
+      </div>
+      <div className={classes.rightPanel}>
+        {products.map((product, index) => (
+          <div 
+            key={product.id} 
+            className={`${classes.donutContainer} ${ index === active ? classes.active : ""}`}
+          >
+            <Image src={product.obrazek} alt={product.opis_obrazka} priority fill/>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
