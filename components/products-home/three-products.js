dbbb3d03a6ea8ad53070ddce0ad821c97443b703
Raven <nowakowski920@gmail.com>
2026-01-13 16:43:57 +0100

    deleting useState description and button and imroving code

--- components/products-home/three-products.js
+++ components/products-home/three-products.js
@@ -4,16 +4,14 @@ import classes from "./three-products.module.css";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 
-import strawberry from '@/assets/strawberry.png';
-import blueberry from '@/assets/blueberry.png';
-import chocolate from '@/assets/chocolate.png';
+import strawberry from "@/assets/strawberry.png";
+import blueberry from "@/assets/blueberry.png";
+import chocolate from "@/assets/chocolate.png";
 const fruitsTable = [strawberry, blueberry, chocolate];
 
-import leaf from '@/assets/leaf.png';
+import leaf from "@/assets/leaf.png";
 const leafTable = [leaf, leaf, leaf];
 
- 
- 
 export default function ThreeProducts({ products }) {
   const [active, setActive] = useState(0);
   const [prevActive, setPrevActive] = useState(0);
@@ -21,35 +19,8 @@ export default function ThreeProducts({ products }) {
   const [linkBackgroundColor, setLinkBackgroundColor] = useState("#EA7BBE");
   const [bg1, setBg1] = useState("rgba(240, 77, 104, 0.55)");
   const [bg2, setBg2] = useState("rgba(255, 255, 255, 1)");
-  const [description, setDescription] = useState(
-    products.map((product, index) => (
-      <p
-        key={product.id}
-        className={
-          index === 0
-            ? `${classes.description} ${classes.active}`
-            : classes.description
-        }
-      >
-        {product.opis}
-      </p>
-    ))
-  );
-  const [donutBtn, setDonutBtn] = useState(
-    products.map((prod, index) => (
-      <button
-        key={prod.id}
-        onClick={() => donutHandler(index)}
-        className={
-          index === 0
-            ? `${classes.donutsBtn} ${classes.active}`
-            : classes.donutsBtn
-        }
-      >
-        <Image src={prod.obrazek} alt={prod.opis_obrazka} priority fill />
-      </button>
-    ))
-  );
+
+ 
 
   const donutHandler = (newIndex) => {
     if (isAnimating) return; // Zablokuj klikanie podczas animacji
@@ -57,20 +28,7 @@ export default function ThreeProducts({ products }) {
     setIsAnimating(true);
     setActive(newIndex);
 
-    setDescription(
-      products.map((product, index) => (
-        <p
-          key={product.id}
-          className={
-            index === newIndex
-              ? `${classes.description} ${classes.active}`
-              : classes.description
-          }
-        >
-          {product.opis}
-        </p>
-      ))
-    );
+     
     if (newIndex === 0) {
       setLinkBackgroundColor("#EA7BBE");
       setBg1("rgba(240, 77, 104, 0.55)");
@@ -84,181 +42,201 @@ export default function ThreeProducts({ products }) {
       setBg1("rgba(151, 79, 45, 0.55)");
       setBg2("rgba(255, 255, 255, 1)");
     }
-
-    setDonutBtn(
-      products.map((prod, index) => (
-        <button
-          key={prod.id}
-          onClick={() => donutHandler(index)}
-          className={
-            index === newIndex
-              ? `${classes.donutsBtn} ${classes.active}`
-              : classes.donutsBtn
-          }
-          //  disabled={index ===}
-        >
-          <Image src={prod.obrazek} alt={prod.opis_obrazka} priority fill />
-        </button>
-      ))
-    );
   };
 
   // Uaktualnij prevActive po tym jak animacja się skończy (1s)
   useEffect(() => {
     const timer = setTimeout(() => {
       setPrevActive(active);
+      setIsAnimating(false);
     }, 1000);
 
     return () => clearTimeout(timer);
   }, [active]);
 
   return (
-    <main
-      className={classes.main}
-      style={{
-        "--bg1": bg1,
-        "--bg2": bg2,
-      }}
-    >
-      <div className={classes.leftPanel}>
-        <div className={classes.viewport}>
-          <div
-            className={classes.slider}
-            style={{ transform: `translateY(-${active * 150}px)` }}
+    <>
+      <main
+        className={classes.main}
+        style={{
+          "--bg1": bg1,
+          "--bg2": bg2,
+        }}
+      >
+        <div className={classes.leftPanel}>
+          <div className={classes.viewport}>
+            <div
+              className={classes.slider}
+              style={{ transform: `translateY(-${active * 150}px)` }}
+            >
+              {products.map((product) => (
+                <h1
+                  key={product.id}
+                  className={`${classes.slide} ${"balooFont"}`}
+                >
+                  {product.nazwa}
+                </h1>
+              ))}
+            </div>
+          </div>
+
+          <div className={classes.descriptionBox}>
+            {products.map((product, index) => (
+            <p
+              key={product.id}
+              className={
+                index === active
+                  ? `${classes.description} ${classes.active}`
+                  : classes.description
+              }
           >
-            {products.map((product) => (
-              <h1
-                key={product.id}
-                className={`${classes.slide} ${"balooFont"}`}
+            {product.opis}
+          </p>
+        ))}
+          </div>
+
+          <div className={classes.links}>
+            <Link
+              href="/"
+              style={{ backgroundColor: `${linkBackgroundColor}` }}
+            >
+              Zamów teraz
+            </Link>
+            <Link
+              href="/"
+              style={{ backgroundColor: `${linkBackgroundColor}` }}
+            >
+              Więcej
+            </Link>
+          </div>
+
+          <div className={classes.donutsBtnsContainer}>
+            {products.map((prod, index) => (
+              <button
+                key={prod.id}
+                onClick={() => donutHandler(index)}
+                className={
+                  index === active
+                    ? `${classes.donutsBtn} ${classes.active}`
+                    : classes.donutsBtn
+                }
+                disabled={isAnimating || prevActive === index}
               >
-                {product.nazwa}
-              </h1>
+                <Image
+                  src={prod.obrazek}
+                  alt={prod.opis_obrazka}
+                  priority
+                  fill
+                />
+              </button>
             ))}
           </div>
         </div>
-
-        <div className={classes.descriptionBox}>{description}</div>
-
-        <div className={classes.links}>
-          <Link href="/" style={{ backgroundColor: `${linkBackgroundColor}` }}>
-            Zamów teraz
-          </Link>
-          <Link href="/" style={{ backgroundColor: `${linkBackgroundColor}` }}>
-            Więcej
-          </Link>
-        </div>
-
-        <div className={classes.donutsBtnsContainer}>{donutBtn}</div>
-      </div>
-      <div className={classes.rightPanel}>
-        {products.map((product, index) => (
-          <div
-            key={product.id}
-            className={`${classes.donutContainer} ${
-              index === active
-                ? classes.active
-                : index === prevActive
-                ? classes.reverse
-                : ""
-            }`}
-          >
-            <Image
-              src={product.obrazek}
-              alt={product.opis_obrazka}
-              priority
-              fill
-            />
-          </div>
-        ))}
-
-        {fruitsTable.map((fruit, index) => (
+        <div className={classes.rightPanel}>
+          {products.map((product, index) => (
             <div
-              key={index} 
+              key={product.id}
+              className={`${classes.donutContainer} ${
+                index === active
+                  ? classes.active
+                  : index === prevActive
+                  ? classes.reverse
+                  : ""
+              }`}
+            >
+              <Image
+                src={product.obrazek}
+                alt={product.opis_obrazka}
+                priority
+                fill
+              />
+            </div>
+          ))}
+
+          {fruitsTable.map((fruit, index) => (
+            <div
+              key={index}
               className={`${classes.fruitOne} ${
                 index === active
-                ? classes.active
-                : index === prevActive
-                ? classes.reverse
-                : ""}
-                `}>
-              <Image 
-                src={fruit.src} 
-                alt={'fruit'} 
-                priority 
-                fill
-              />
-            </div>))}
+                  ? classes.active
+                  : index === prevActive
+                  ? classes.reverse
+                  : ""
+              }
+                `}
+            >
+              <Image src={fruit.src} alt={"fruit"} priority fill />
+            </div>
+          ))}
 
-        {fruitsTable.map((fruit, index) => (
+          {fruitsTable.map((fruit, index) => (
             <div
-              key={index} 
+              key={index}
               className={`${classes.fruitTwo} ${
                 index === active
-                ? classes.active
-                : index === prevActive
-                ? classes.reverse
-                : ""}
-                `}>
-              <Image 
-                src={fruit.src} 
-                alt={'fruit'} 
-                priority 
-                fill
-              />
-            </div>))}
-         {fruitsTable.map((fruit, index) => (
+                  ? classes.active
+                  : index === prevActive
+                  ? classes.reverse
+                  : ""
+              }
+                `}
+            >
+              <Image src={fruit.src} alt={"fruit"} priority fill />
+            </div>
+          ))}
+          {fruitsTable.map((fruit, index) => (
             <div
-              key={index} 
+              key={index}
               className={`${classes.fruitThree} ${
                 index === active
-                ? classes.active
-                : index === prevActive
-                ? classes.reverse
-                : ""}
-                `}>
-              <Image 
-                src={fruit.src} 
-                alt={'fruit'} 
-                priority 
-                fill
-              />
-            </div>))}
-            {leafTable.map((leaf, index) => (
+                  ? classes.active
+                  : index === prevActive
+                  ? classes.reverse
+                  : ""
+              }
+                `}
+            >
+              <Image src={fruit.src} alt={"fruit"} priority fill />
+            </div>
+          ))}
+          {leafTable.map((leaf, index) => (
             <div
-              key={index} 
+              key={index}
               className={`${classes.leafOne} ${
                 index === active
-                ? classes.active
-                : index === prevActive
-                ? classes.reverse
-                : ""}
-                `}>
-              <Image 
-                src={leaf.src} 
-                alt={'leaf'} 
-                priority 
-                fill
-              />
-            </div>))}
-            {leafTable.map((leaf, index) => (
+                  ? classes.active
+                  : index === prevActive
+                  ? classes.reverse
+                  : ""
+              }
+                `}
+            >
+              <Image src={leaf.src} alt={"leaf"} priority fill />
+            </div>
+          ))}
+          {leafTable.map((leaf, index) => (
             <div
-              key={index} 
+              key={index}
               className={`${classes.leafTwo} ${
                 index === active
-                ? classes.active
-                : index === prevActive
-                ? classes.reverse
-                : ""}
-                `}>
-              <Image 
-                src={leaf.src} 
-                alt={'leaf'} 
-                priority 
-                fill
-              />
-            </div>))}     
-        
-      </div>
-    </main>
+                  ? classes.active
+                  : index === prevActive
+                  ? classes.reverse
+                  : ""
+              }
+                `}
+            >
+              <Image src={leaf.src} alt={"leaf"} priority fill />
+            </div>
+          ))}
+        </div>
+      </main>
+      <section className={classes.section}>
+        <h2>Co nas wyróżnia?</h2>
+        <p>Rzemieślnicze donuty wypiekane codziennie</p>
+        <p>Tradycyjna receptura</p>
+        <p>Lokalna produkcja</p>
+      </section>
+    </>
   );
 }
