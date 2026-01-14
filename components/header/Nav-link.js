ae93d0ac40cf4e00301244076f7cb0e3b5ec54d3
Lucekkk <nowakowski920@gmail.com>
2026-01-15 00:33:20 +0100

    Update Nav-link.js

--- components/header/Nav-link.js
+++ components/header/Nav-link.js
@@ -2,7 +2,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import classes from "./nav-link.module.css";
+import classes from "./Nav-link.module.css";
 
 export default function NavLink({ href, children }) {
   const path = usePathname();
